d4450bfc1c068c2fbf4f5763fad10e30836cf725
shiran-trrzs <shirannnn.4@gmail.com>
2022-04-18 18:59:28 -0500

    Add: install body-parser

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
-let mysql = require('mysql');
+const mysql = require('mysql');
+const express = require('express');
 
-let connection = mysql.createConnection({
+const app = express();
+
+app.use(bodyParse().json());
+
+const connection = mysql.createConnection({
     host     : 'mdb-test.c6vunyturrl6.us-west-1.rds.amazonaws.com',
     user     : 'bsale_test',
     password : 'bsale_test',
