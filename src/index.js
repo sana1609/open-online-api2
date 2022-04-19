3601db6f98f7cbcdb381a6c999f0599dbadc0eb2
shiran-trrzs <shirannnn.4@gmail.com>
2022-04-18 22:53:46 -0500

    Feat: create get request to get product and category db

--- src/index.js
+++ src/index.js
@@ -8,31 +8,103 @@ const PORT = process.env.PORT || 3050
 
 app.use(bodyParser.json());
 
-const connection = mysql.createConnection({
-    host     : 'mdb-test.c6vunyturrl6.us-west-1.rds.amazonaws.com',
-    user     : 'bsale_test',
-    password : 'bsale_test',
-    database : 'bsale_test'
+
+    const connection = mysql.createConnection({
+        host: 'mdb-test.c6vunyturrl6.us-west-1.rds.amazonaws.com',
+        user: 'bsale_test',
+        password: 'bsale_test',
+        database: 'bsale_test'
+    });
+
+    // connection.on('error', function(err) {
+    //     console.log(err.code);
+    //   });
+
+
+// Keep alive server
+setInterval(function () {
+    connection.query('SELECT 1');
+}, 5000);
+
+// const pool = mysql.createPool({
+//     host: 'mdb-test.c6vunyturrl6.us-west-1.rds.amazonaws.com',
+//     user: 'bsale_test',
+//     password: 'bsale_test',
+//     database: 'bsale_test'
+//   });
+  
+//   // ... later
+//   pool.query('select 1 + 1', function (error, results, fields) {
+//     if (error) throw error;
+//     console.log('keeping connection');
+//   }
+//   );
+
+//Routes
+app.get('/', (req, res) => {
+    res.send('Welcome')
 });
 
+app.get('/category', (req, res) => {
+    const sql = 'SELECT * from category';
 
-connection.connect(function(error){
+    connection.query(sql, function (error, result) {
+        if (error)
+            throw error;
+
+        if (result.length > 0) {
+            res.json(result);
+        } else {
+            res.send('Not result')
+        };
+    })
+});
+
+app.get('/product', (req, res) => {
+    const sql = 'SELECT * from product';
+
+    connection.query(sql, function (error, result) {
+        if (error)
+            throw error;
+
+        if (result.length > 0) {
+            res.json(result);
+        } else {
+            res.send('Not result')
+        };
+    })
+});
+
+app.post('/', (req, res) => {
+    res.send('')
+});
+
+app.put('/', (req, res) => {
+    res.send('')
+});
+
+app.delete('/', (req, res) => {
+    res.send('')
+});
+
+// Check connect
+connection.connect(function (error) {
     if (error) {
         throw error;
     } else {
-        console.log('succesful connection');
+        console.log('successful connection');
     }
 });
 
-connection.query('SELECT * from product', function(error, result, fields) {
-    if(error)
-    throw error;
+// connection.query('SELECT * from product', function(error, result, fields) {
+//     if(error)
+//     throw error;
 
-    result.forEach(res => {
-        console.log(res);
-    });
-})
+//     result.forEach(res => {
+//         console.log(res);
+//     });
+// })
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-connection.end();
+// connection.end();
